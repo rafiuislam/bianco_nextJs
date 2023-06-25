5f5d1d19cdacd4ed407a6d1eb7703b27fb63323e
Rafiu Islam <rafiuislam123@gmail.com>
2023-06-25 16:08:14 +0600

    fixed the animation in biancaffe, so there is no y-scroll while page load

--- components/homeSections/Biancaffe.jsx
+++ components/homeSections/Biancaffe.jsx
@@ -2,9 +2,10 @@
 import React from "react";
 import Image from "next/image";
 import SlideUp from "../animate/SlideUp";
+import SlideDown from "../animate/SlideDown";
 import imgBian from "../../public/img/Take-Your-Time-bg.jpg";
 import imgBian2 from "../../public/img_hero/Slider-4.jpg";
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 
 // const slides = [
 //   {
@@ -31,61 +32,61 @@ const Biancaffe = () => {
       </SlideUp>
       <div className="flex items-center justify-center overflow-y-hidden">
         <div className="grid grid-cols-1 gap-5 md:grid-cols-2 overflow-y-hidden">
-          {/* <SlideFromLeft offset="-300px 0px -300px 0px"> */}
-          <div className="group overflow-y-hidden relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
-            <div className="h-98 w-98">
-              <Image
-                className="h-full w-full overflow-y-hidden object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
-                src={imgBian}
-                alt=""
-              />
-            </div>
-            <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
-            <div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
-              <h1 className="font-custom text-3xl font-bold text-white">
-                Cafe
-              </h1>
-              <p className="sm:p-2 mb-3 text-lg font-custom text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
-                dolore adipisci placeat.
-              </p>
-              <div className="flex justify-center items-center">
-                <button className="mt-4 font-semibold text-base font-raleway rounded active:bg-primary/80 relative -top-1 -left-1 bg-secondary py-2.5 px-5 uppercase text-white transition-all before:absolute before:top-1 before:left-1 before:-z-[1] before:h-full before:w-full before:border-2 before:border-secondary before:transition-all before:content-[''] active:top-0 active:left-0 before:active:top-0 before:active:left-0">
-                  See More
-                </button>
+          <SlideDown offset="-300px 0px -300px 0px">
+            <div className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
+              <div className="h-98 w-98">
+                <Image
+                  className="h-full w-full overflow-y-hidden object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
+                  src={imgBian}
+                  alt=""
+                />
               </div>
-              {/* <button className="rounded-full bg-primary text-white font-raleway font-medium text-sm py-2 px-3.5 shadow shadow-black/60">
+              <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+              <div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+                <h1 className="font-custom text-3xl font-bold text-white">
+                  Cafe
+                </h1>
+                <p className="sm:p-2 mb-3 text-lg font-custom text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Facilis dolore adipisci placeat.
+                </p>
+                <div className="flex justify-center items-center">
+                  <button className="mt-4 font-semibold text-base font-raleway rounded active:bg-primary/80 relative -top-1 -left-1 bg-secondary py-2.5 px-5 uppercase text-white transition-all before:absolute before:top-1 before:left-1 before:-z-[1] before:h-full before:w-full before:border-2 before:border-secondary before:transition-all before:content-[''] active:top-0 active:left-0 before:active:top-0 before:active:left-0">
+                    See More
+                  </button>
+                </div>
+                {/* <button className="rounded-full bg-primary text-white font-raleway font-medium text-sm py-2 px-3.5 shadow shadow-black/60">
                 See More
               </button> */}
-            </div>
-          </div>
-          {/* </SlideFromLeft> */}
-          {/* <SlideFromRight offset="-300px 0px -300px 0px"> */}
-          <div className="group overflow-y-hidden relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
-            <div className="h-98 w-98">
-              <Image
-                className="h-full w-full overflow-y-hidden object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
-                src={imgBian2}
-                alt=""
-              />
-            </div>
-            <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
-            <div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
-              <h1 className="font-custom text-3xl font-bold text-white">
-                Shop
-              </h1>
-              <p className="sm:p-2 mb-3 text-lg font-custom text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
-                dolore adipisci placeat.
-              </p>
-              <div className="flex justify-center items-center">
-                <button className="mt-4 font-semibold text-base font-raleway rounded active:bg-primary/80 relative -top-1 -left-1 bg-secondary py-2.5 px-5 uppercase text-white transition-all before:absolute before:top-1 before:left-1 before:-z-[1] before:h-full before:w-full before:border-2 before:border-secondary before:transition-all before:content-[''] active:top-0 active:left-0 before:active:top-0 before:active:left-0">
-                  See More
-                </button>
               </div>
             </div>
-          </div>
-          {/* </SlideFromRight> */}
+          </SlideDown>
+          <SlideDown offset="-300px 0px -300px 0px">
+            <div className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
+              <div className="h-98 w-98">
+                <Image
+                  className="h-full w-full overflow-y-hidden object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
+                  src={imgBian2}
+                  alt=""
+                />
+              </div>
+              <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+              <div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+                <h1 className="font-custom text-3xl font-bold text-white">
+                  Shop
+                </h1>
+                <p className="sm:p-2 mb-3 text-lg font-custom text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Facilis dolore adipisci placeat.
+                </p>
+                <div className="flex justify-center items-center">
+                  <button className="mt-4 font-semibold text-base font-raleway rounded active:bg-primary/80 relative -top-1 -left-1 bg-secondary py-2.5 px-5 uppercase text-white transition-all before:absolute before:top-1 before:left-1 before:-z-[1] before:h-full before:w-full before:border-2 before:border-secondary before:transition-all before:content-[''] active:top-0 active:left-0 before:active:top-0 before:active:left-0">
+                    See More
+                  </button>
+                </div>
+              </div>
+            </div>
+          </SlideDown>
         </div>
       </div>
     </section>
--- pages/index.jsx
+++ pages/index.jsx
@@ -11,7 +11,7 @@ export default function Home() {
     <div className="bg-bg-h overflow-y-hidden">
       <Slider />
 
-      {/* <Biancaffe /> */}
+      <Biancaffe />
       <Shop />
 
       <About />
