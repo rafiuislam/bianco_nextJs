6b378786c01c35e752405ceecd6e3d7547b443c8
Rafiu Islam <rafiuislam123@gmail.com>
2023-07-13 15:05:42 +0600

    removed extra import biancaffe2

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,7 +4,6 @@ import About from "../components/homeSections/About";
 import Instagram from "../components/homeSections/Instagram";
 import Shop from "../components/homeSections/Shop";
 import Biancaffe from "../components/homeSections/Biancaffe";
-// import Biancaffe2 from "../components/homeSections/Biancaffe2";
 
 //HomePage
 export default function Home() {
@@ -12,7 +11,6 @@ export default function Home() {
     <div className="bg-bg-h overflow-y-hidden">
       <Hero />
       <Biancaffe />
-      {/* <Biancaffe2 /> */}
       <Shop />
       <About />
       <Instagram />
