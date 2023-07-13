445fb5d18690ca18e9c3912f2770345718b64295
Rafiu Islam <rafiuislam123@gmail.com>
2023-07-13 15:08:31 +0600

    removed react-messenger-customer-chat

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,11 @@
 import Footer from "./Footer";
 import Navbar from "./Navbar";
-import MessengerCustomerChat from "react-messenger-customer-chat";
 
 const Layout = ({ children }) => {
   return (
     <>
       <Navbar />
       {children}
-      <MessengerCustomerChat pageId="<PAGE_ID>" appId="<APP_ID>" />
       <Footer />
     </>
   );
