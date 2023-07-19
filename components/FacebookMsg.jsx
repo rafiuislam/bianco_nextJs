8bd20386971d26f1a2611a29d7db1e78f72d132e
Rafiu Islam <rafiuislam123@gmail.com>
2023-07-19 12:44:20 +0600

    color change in chatbox

--- components/FacebookMsg.jsx
+++ components/FacebookMsg.jsx
@@ -5,7 +5,11 @@ import { FacebookProvider, CustomChat } from "react-facebook";
 const FacebookMsg = () => {
   return (
     <FacebookProvider appId="1055942685771723" chatSupport>
-      <CustomChat pageId="103302306108226" minimized={false} />
+      <CustomChat
+        pageId="103302306108226"
+        minimized={true}
+        className="bg-primary"
+      />
     </FacebookProvider>
   );
 };
